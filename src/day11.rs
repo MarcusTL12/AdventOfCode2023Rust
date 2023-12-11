354c3f633bf643b6bc97c22058235da408c47944
MarcusTL12 <marcuspaafjellet@gmail.com>
2023-12-11 19:19:15 +0100

    O(n) works

--- src/day11.rs
+++ src/day11.rs
@@ -1,6 +1,6 @@
-use std::fs::read_to_string;
+use std::{fs::read_to_string, time::Instant};
 
-use ndarray::{s, ArrayView2};
+use ndarray::{s, ArrayView1, ArrayView2};
 
 pub const PARTS: [fn(&str); 2] = [part1, part2];
 
@@ -68,10 +68,55 @@ fn find_tot_dist(path: &str, expand: usize) -> usize {
         .sum()
 }
 
+fn get_galaxy_coords<'a, I: Iterator<Item = ArrayView1<'a, u8>>>(
+    col_iter: I,
+    expand: usize,
+) -> Vec<usize> {
+    col_iter
+        .scan(0, |x, col| {
+            *x += 1;
+            if col.iter().all(|&c| c == b'.') {
+                *x += expand;
+            }
+            Some((*x, col))
+        })
+        .flat_map(|(x, col)| {
+            col.into_iter()
+                .filter_map(move |&c| (c == b'#').then_some(x))
+        })
+        .collect()
+}
+
+fn get_sum_dists(coords: &[usize]) -> usize {
+    let l = coords.len();
+
+    let (first, last) = coords.split_at(l / 2);
+
+    first
+        .iter()
+        .zip(last.iter().rev())
+        .scan(l + 1, |c, x| {
+            *c -= 2;
+            Some((*c, x))
+        })
+        .map(|(c, (&a, &b))| c * a.abs_diff(b))
+        .sum()
+}
+
+fn find_tot_dist2(path: &str, expand: usize) -> usize {
+    let input = read_to_string(path).unwrap();
+    let grid = parse_input(&input);
+
+    let galaxy_xs = get_galaxy_coords(grid.columns().into_iter(), expand);
+    let galaxy_ys = get_galaxy_coords(grid.rows().into_iter(), expand);
+
+    get_sum_dists(&galaxy_xs) + get_sum_dists(&galaxy_ys)
+}
+
 fn part1(path: &str) {
     println!("{}", find_tot_dist(path, 1));
 }
 
 fn part2(path: &str) {
-    println!("{}", find_tot_dist(path, 1000000 - 1));
+    println!("{}", find_tot_dist2(path, 1000000 - 1));
 }
