0cd92001cdcb8983b623b24eebee2b8c2b0cd2e6
MarcusTL12 <marcuspaafjellet@gmail.com>
2023-12-16 17:54:09 +0100

    day 16

--- src/day16.rs
+++ src/day16.rs
@@ -1,88 +1,129 @@
+use ndarray::{s, Array2, ArrayView2};
+
 pub const PARTS: [fn(&str); 2] = [part1, part2];
 
-#[derive(Debug)]
-enum Reflector {
-    MirrorRight,
-    MirrorLeft,
-    SplitHorizontal,
-    SplitVertical,
+fn parse_input(input: &str) -> ArrayView2<u8> {
+    let b = input.as_bytes();
+
+    let w = b.split(|&x| x == b'\n').next().unwrap().len() + 1;
+    let h = b.len() / w;
+
+    ArrayView2::from_shape((h, w), b)
+        .unwrap()
+        .slice_move(s![0..h, 0..(w - 1)])
 }
 
-impl From<char> for Reflector {
-    fn from(value: char) -> Self {
-        use Reflector::*;
-        match value {
-            '\\' => MirrorRight,
-            '/' => MirrorLeft,
-            '-' => SplitHorizontal,
-            '|' => SplitVertical,
-            _ => panic!(),
-        }
+fn shift_pos([y, x]: [usize; 2], dir: usize) -> [usize; 2] {
+    match dir {
+        0 => [y, x + 1],
+        1 => [y + 1, x],
+        2 => [y, x.wrapping_sub(1)],
+        3 => [y.wrapping_sub(1), x],
+        _ => panic!(),
     }
 }
 
-#[derive(Debug)]
-struct Node {
-    pub reflector: Reflector,
-    pub pos: [usize; 2],
-    pub connections: [Option<usize>; 4],
-}
-
-type Edge = [Option<usize>; 2];
-
-fn parse_input(input: &str) -> (Vec<Node>, [Vec<Edge>; 2]) {
-    let mut top = Vec::new();
-
-    let mut nodes = Vec::<Node>::new();
-
-    let mut hlines = Vec::new();
-    let mut vlines = Vec::new();
-
-    for (y, l) in input.lines().enumerate() {
-        if top.is_empty() {
-            top.resize(l.len(), None);
-        }
-
-        let mut left = None;
-
-        for (x, c) in l.chars().enumerate().filter(|&(_, c)| c != '.') {
-            let node = Node {
-                reflector: Reflector::from(c),
-                pos: [x, y],
-                connections: [left, top[x], None, None],
-            };
-            let i = Some(nodes.len());
-
-            hlines.push([left, i]);
-            vlines.push([top[x], i]);
-
-            if let Some(j) = left {
-                nodes[j].connections[2] = i;
-            }
-
-            if let Some(j) = top[x] {
-                nodes[j].connections[3] = i;
-            }
-
-            left = i;
-            top[x] = i;
-            nodes.push(node);
-        }
+fn find_energized(
+    grid: ArrayView2<u8>,
+    energized: &mut Array2<u8>,
+    mut pos: [usize; 2],
+    dir: usize,
+) {
+    if energized
+        .get(pos)
+        .map(|e| e & (1 << dir) != 0)
+        .unwrap_or(true)
+        && matches!(grid.get(pos), Some(b'.'))
+    {
+        return;
     }
 
-    (nodes, [hlines, vlines])
+    while let Some(b'.') = grid.get(pos) {
+        energized[pos] |= 1 << dir;
+        pos = shift_pos(pos, dir);
+    }
+
+    match (grid.get(pos), dir) {
+        (Some(b'|'), 1 | 3) | (Some(b'-'), 0 | 2) => {
+            energized[pos] |= 1 << dir;
+            find_energized(grid, energized, shift_pos(pos, dir), dir);
+        }
+        (Some(b'|'), _) => {
+            energized[pos] |= (1 << 1) | (1 << 3);
+            find_energized(grid, energized, shift_pos(pos, 1), 1);
+            find_energized(grid, energized, shift_pos(pos, 3), 3);
+        }
+        (Some(b'-'), _) => {
+            energized[pos] |= 1 | (1 << 2);
+            find_energized(grid, energized, shift_pos(pos, 0), 0);
+            find_energized(grid, energized, shift_pos(pos, 2), 2);
+        }
+        (Some(b'\\' | b'/'), _) => {
+            let ndir = match grid.get(pos) {
+                Some(b'\\') => [1, 0, 3, 2],
+                Some(b'/') => [3, 2, 1, 0],
+                _ => unreachable!(),
+            }[dir];
+
+            energized[pos] |= 1 << ndir;
+            find_energized(grid, energized, shift_pos(pos, ndir), ndir);
+        }
+        _ => {}
+    }
 }
 
 fn part1(input: &str) {
-    let (nodes, [hlines, vlines]) = parse_input(input);
+    let grid = parse_input(input);
+    let mut energized = Array2::from_elem(grid.dim(), 0);
 
-    println!("{} {} {}", nodes.len(), hlines.len(), vlines.len());
+    find_energized(grid, &mut energized, [0, 0], 0);
 
-    // println!("{nodes:#?}");
-    // println!("\n{hlines:?}");
-    // println!("\n{vlines:?}");
+    let ans = energized.iter().filter(|&&c| c != 0).count();
+
+    println!("{ans}");
 }
 
 fn part2(input: &str) {
-    parse_input(input);
+    let grid = parse_input(input);
+    let (h, w) = grid.dim();
+    let mut energized = Array2::from_elem(grid.dim(), 0);
+
+    let ans = (0..h)
+        .map(|y| {
+            find_energized(grid, &mut energized, [y, 0], 0);
+            let a = energized.iter().filter(|&&c| c != 0).count();
+            for c in energized.iter_mut() {
+                *c = 0;
+            }
+            find_energized(grid, &mut energized, [y, w - 1], 2);
+            let b = energized.iter().filter(|&&c| c != 0).count();
+            for c in energized.iter_mut() {
+                *c = 0;
+            }
+
+            a.max(b)
+        })
+        .max()
+        .unwrap();
+
+    let ans = (0..w)
+        .map(|x| {
+            find_energized(grid, &mut energized, [0, x], 1);
+            let a = energized.iter().filter(|&&c| c != 0).count();
+            for c in energized.iter_mut() {
+                *c = 0;
+            }
+            find_energized(grid, &mut energized, [h - 1, x], 3);
+            let b = energized.iter().filter(|&&c| c != 0).count();
+            for c in energized.iter_mut() {
+                *c = 0;
+            }
+
+            a.max(b)
+        })
+        .max()
+        .unwrap()
+        .max(ans);
+
+    println!("{ans}");
 }
